a7ae7b81517f93aecb02e6da52dee5f0c7684aec
Vihan-S <110139029+Vihan-S@users.noreply.github.com>
2023-08-26 16:16:40 -0700

    Update kwitter_room.js

---
+++ kwitter_room.js
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCu1tVsE8hKK251dPJWyq26H9gPK_uDCdM",
+  authDomain: "lets-chat-web-app-c5ef9.firebaseapp.com",
+  projectId: "lets-chat-web-app-c5ef9",
+  storageBucket: "lets-chat-web-app-c5ef9.appspot.com",
+  messagingSenderId: "883671299592",
+  appId: "1:883671299592:web:73cd6ffb11566f162f83bd"
+};
